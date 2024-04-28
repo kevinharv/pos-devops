353ffeba248bd0b880b8892bf5169f0f45fe4325
kevinharv <kharvey2002@gmail.com>
2024-04-27 19:07:08 -0500

    feat: DB schema

--- server/migrations/1_CREATE_TABLES.sql
+++ server/migrations/1_CREATE_TABLES.sql
@@ -1,1 +1,88 @@
--- Create tables for POS server application
+-- Create items table
+CREATE TABLE IF NOT EXISTS items (
+    itemID          BIGSERIAL    UNIQUE NOT NULL,
+    categoryID      BIGINT       NOT NULL,
+    itemName        VARCHAR(32)  NOT NULL,
+    itemDescription VARCHAR(255),
+    price           MONEY        NOT NULL,
+
+    created_at  TIMESTAMP  DEFAULT now(),
+    
+    PRIMARY KEY (itemID)
+);
+
+-- Create stores table
+CREATE TABLE IF NOT EXISTS stores (
+    storeID     BIGSERIAL    UNIQUE NOT NULL,
+    storeName   VARCHAR(255) NOT NULL,
+
+    created_at  TIMESTAMP  DEFAULT now(),
+    
+    PRIMARY KEY (storeID)
+);
+
+-- Create users table
+CREATE TABLE IF NOT EXISTS users (
+    userID      BIGSERIAL    UNIQUE NOT NULL,
+    firstName   VARCHAR(64),
+    middleName  VARCHAR(64),
+    lastName    VARCHAR(64),
+    birthDate   DATE,
+
+    created_at  TIMESTAMP  DEFAULT now(),
+
+    PRIMARY KEY (userID)
+);
+
+-- Create payments table
+CREATE TABLE IF NOT EXISTS payments (
+    paymentID   BIGSERIAL    UNIQUE NOT NULL,
+    cardholder  VARCHAR(255) NOT NULL,
+    cardnumber  VARCHAR(16)  NOT NULL,
+    expiration  DATE         NOT NULL,
+
+    created_at  TIMESTAMP  DEFAULT now(),
+
+    PRIMARY KEY (paymentID)
+);
+
+-- Create POS table
+CREATE TABLE IF NOT EXISTS pos (
+    posID    BIGSERIAL UNIQUE NOT NULL,
+    storeID  BIGINT    NOT NULL,
+    hostname VARCHAR(32),
+
+    created_at  TIMESTAMP  DEFAULT now(),
+
+    PRIMARY KEY (posID),
+    FOREIGN KEY (storeID) REFERENCES stores(storeID)
+        ON UPDATE CASCADE
+        ON DELETE CASCADE
+);
+
+-- Create transactions table
+CREATE TABLE IF NOT EXISTS transactions (
+    transactionID BIGSERIAL UNIQUE NOT NULL,
+    posID         BIGINT    NOT NULL,
+    storeID       BIGINT,
+    userID        BIGINT,
+    paymentID     BIGINT,
+    startTime     TIMESTAMP,
+    endTime       TIMESTAMP,
+
+    created_at  TIMESTAMP  DEFAULT now(),
+
+    PRIMARY KEY (transactionID),
+    FOREIGN KEY (posID) REFERENCES pos(posID)
+        ON UPDATE CASCADE
+        ON DELETE SET NULL,
+    FOREIGN KEY (storeID) REFERENCES stores(storeID)
+        ON UPDATE CASCADE
+        ON DELETE SET NULL,
+    FOREIGN KEY (userID) REFERENCES users(userID)
+        ON UPDATE CASCADE
+        ON DELETE NO ACTION,
+    FOREIGN KEY (paymentID) REFERENCES payments(paymentID)
+        ON UPDATE CASCADE
+        ON DELETE SET NULL
+);
--- server/migrations/2_TEST_DATA.sql
+++ server/migrations/2_TEST_DATA.sql
@@ -1,1 +1,21 @@
--- insert test data
+-- Create some items
+INSERT INTO items (categoryID, itemName, itemDescription, price) VALUES (1, 'Dell XPS 13', 'Laptop Computer', 1499.99);
+
+-- Create some stores
+INSERT INTO stores (storeName) VALUES ('Store 1');
+INSERT INTO stores (storeName) VALUES ('Store 2');
+INSERT INTO stores (storeName) VALUES ('Store 3');
+
+-- Create some users
+INSERT INTO users (firstName, middleName, lastName, birthDate) VALUES ('Joseph', 'Lee', 'Lin', '2001/07/07');
+
+-- Create some payments
+INSERT INTO payments (cardholder, cardnumber, expiration) VALUES ('Foo A Bar', '123456789098765', '2026/04/01');
+
+-- Create some POS terminals
+INSERT INTO pos (storeID, hostname) VALUES (1, 'S1POS1');
+INSERT INTO pos (storeID, hostname) VALUES (1, 'S1POS2');
+INSERT INTO pos (storeID, hostname) VALUES (2, 'S2POS1');
+INSERT INTO pos (storeID, hostname) VALUES (2, 'S2POS2');
+INSERT INTO pos (storeID, hostname) VALUES (3, 'S3POS1');
+INSERT INTO pos (storeID, hostname) VALUES (3, 'S3POS2');
