0c1fd4cef06e421cee2aba1e472274d5ee454548
kevinharv <kharvey2002@gmail.com>
2024-07-04 17:09:52 -0500

    added transactions to routes

--- server/routes/routes.go
+++ server/routes/routes.go
@@ -23,8 +23,8 @@ func AddRoutes(
 	mux.Handle("POST /v1/transaction/start", transaction.StartTransaction(logger, db))
 	mux.Handle("PUT /v1/transaction/item/add", transaction.AddItemToTransaction(logger, db))
 	mux.Handle("DELETE /v1/transaction/item/remove", transaction.RemoveItemFromTransaction(logger, db))
-	mux.Handle("POST /v1/transaction/checkout/start", http.NotFoundHandler())
-	mux.Handle("POST /v1/transaction/checkout/payment", http.NotFoundHandler())
+	mux.Handle("POST /v1/transaction/checkout/start", transaction.StartCheckout(logger, db))
+	mux.Handle("POST /v1/transaction/checkout/payment", transaction.CollectPayment(logger, db))
 	mux.Handle("POST /v1/transaction/close", transaction.CloseTransaction(logger, db))
 
 	mux.Handle("GET /v1/item/{id}", item.ItemByID(logger, db))
